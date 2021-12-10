c3f7fc5fab24898f1a9bcf3b69686d034c2e1173
Klimova Elena <helen_zlat@mail.ru>
2021-12-10 17:11:22 +0300

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import main.java.animals.Kotik;
+
+public class Application {
+    public static void main(String[] args) {
+        Kotik cat1 = new Kotik("cat1", "meow", 10, 2.5f);
+        Kotik cat2 = new Kotik();
+        cat2.setName("cat2");
+        cat2.setVoice("meow");
+        cat2.setSatiety(10);
+        cat2.setWeight(2.5f);
+
+        String[] temp = cat1.liveAnotherDay();
+        for (String s : temp) {
+            System.out.println(s);
+        }
+        System.out.printf("Name: %s%n Weight: %f%n", cat2.getName(), cat2.getWeight());
+        if (compareVoice(cat1, cat2)) {
+            System.out.println("Коты одинаково разговаривают");
+        }
+        System.out.println("Количество созданных экземпляров: " + Kotik.count);
+    }
+
+    public static boolean compareVoice(Kotik cat1, Kotik cat2) {
+        return cat1.getVoice().equals(cat2.getVoice());
+    }
+}
